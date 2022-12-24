5aea0bebf02578af57134312f398161d94c56c8a
Arihant Jain <95957656+a-k-j@users.noreply.github.com>
2022-12-25 02:00:04 +0530

    Version 1
    Uploaded all the files

---
+++ index.js
@@ -0,0 +1,22 @@
+document.querySelector('.btn').onclick = function() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+    if (randomNumber1 > randomNumber2)
+    {
+        document.querySelector(".who-won").innerText = "Player 1 Wins!!!";
+    }
+
+    else if (randomNumber1 < randomNumber2)
+    {
+        document.querySelector(".who-won").innerText = "Player 2 Wins!!!";
+    }
+
+    else
+    {
+        document.querySelector(".who-won").innerText = "Aww a draw occured, try again!!!";
+    }
+}
